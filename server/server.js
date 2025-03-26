7740920520d990ffe0fe2b24e5ccc16f39d4c881
Dheeraj-485 <dheerajangural781@gmail.com>
2025-03-26 12:35:19 +0530

    new

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://algo-root-task.netlify.app",
+    origin: ["https://algo-root-task.netlify.app", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
